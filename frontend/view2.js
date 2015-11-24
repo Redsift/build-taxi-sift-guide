b03ae292cdc3588480d7446578f64746f84259aa
Christos Vontas <christos@redsift.io>
2015-11-24 18:21:50 +0000

    margin changes for chart

--- frontend/view2.js
+++ frontend/view2.js
@@ -54,7 +54,7 @@ function _formatPresentationAxis(_d) {
 function _createTaxiChart(container, data) {
 	var svg = dimple.newSvg(container, '100%', 180);
 	var chart = new dimple.chart(svg, data);
-	chart.setMargins(chart.x, 20, 0, 20);
+	chart.setMargins(0, 20, 0, 20);
 	var xAxis = chart.addCategoryAxis('x', 'Display');
 	xAxis.addGroupOrderRule('Date');
 	xAxis.addOrderRule('Date');
