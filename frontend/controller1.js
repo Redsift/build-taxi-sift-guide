48c4be7acf52b369f85d5ce59ca29df1a031970e
Christos Vontas <christos@redsift.io>
2016-02-04 12:43:37 +0000

    Updated view to latest template.

--- frontend/controller1.js
+++ frontend/controller1.js
@@ -1,53 +1,121 @@
 /**
- * Frontend entry point
- *
- * Copyright (c) 2015 Redsift Limited. All rights reserved.
+ * Hello Sift. Frontend entry point.
  */
+
 'use strict';
+/* globals document, Sift */
 
-include('redsift.js');
-
+// Function: loadView
+// Description: Invoked by the Redsift client when a Sift has transitioned to its final size class
+//
+// Parameters:
+// @value: {
+//          sizeClass: {
+//            previous: {width: 'compact'|'full', height: 'compact'|'full'},
+//            current:  {width: 'compact'|'full', height: 'compact'|'full'}
+//          },
+//          type: 'compose'|'email-detail'|'summary',
+//          data: {<object>}
+//        }
+//
+// return: null or {html:'<string>', data: {<object>}}
+// @resolve: function ({html:'<string>', data: {<object>})
+// @reject: function (error)
 Sift.Controller.loadView = function (value, resolve, reject) {
-  console.log('taxi-test: load', value);
-  var html;
-  var data;
-  if(value.sizeClass.current.height === 'list') {
+  console.log('hello-sift: loadView', value);
 
-    html = null;
-    data = {
-      title: 'Example Title',
-      subtitle: 'Example subtitle',
-      image: {
-        url: 'assets/redsift-logo.png'
-      }
-    };
+  /*
+  * Replace example code with your sift logic
+  */
+  var height = value.sizeClass.current.height,
+    response = { data: {}};
+
+  var msg = 'returned synchronously';
+  if(height === 'full') {
+    // Asynchronous return
+    msg = 'waiting for async response...';
+    fullAsyncHandler(resolve, reject);
   }
-  else {
-    var msg = 'returned synchronously';
-    if(value.sizeClass.current.height === 'full') {
-      msg = 'waiting for async response...';
-      setTimeout(function () {
-        // Asynchronous resolve
-        resolve ({
-          data: {message: 'resolved asynchronously'}
-        });
-      }, 1500);
-    }
-    // TODO: return something asynchronously here
-    html = 'frontend/view1.html';
-    data = {
-        sizeClass: value.sizeClass.current,
-        type: value.type,
-        message: msg
-    };
-  }
+
   // Synchronous return
-  return {
-    data: data,
-    html: html
+  response.html = 'frontend/view1.html';
+  response.data = {
+      message: msg
   };
+
+  return response;
 };
 
-Sift.Controller.loadLabel = function (value, resolve, reject) {
-  return {data: 'example-sift'};
+// Function: loadData
+// Description: Invoked by the Sift view to load more data
+//
+// Parameters:
+// @value: <object>
+//
+// return: <object>
+// @resolve: function (<object>)
+// @reject: function (error)
+Sift.Controller.loadData = function (value) {
+  console.log('hello-sift: loadData', value);
+  return Sift.Storage.get({
+      bucket: 'count',
+      keys: [value.key]
+  }).then(function (values) {
+    console.log('hello-sift: storage returned: ', values);
+    return values[0].value;
+  });
 };
+
+// Function: loadLabel
+// Description: Invoked when the Redsift client requires a textual representation for the sift
+//
+// Parameters:
+// @value: {}
+//
+// return: null or {data: 'label string'}
+// @resolve: function ({data: 'label string'})
+// @reject: function (error)
+Sift.Controller.loadLabel = function(value, resolve, reject) {
+  console.log('sift-taxi: loadLabel');
+  return {data: 'Hello Sift'};
+};
+
+// Event: storage update
+// Description: fired whenever anything changes in the Sift's storage
+//              you can replace '*' by the name of a specific bucket
+Sift.Storage.addUpdateListener('*', function (value) {
+  console.log('hello-sift: storage updated: ', value);
+  // Storage has been updated, fetch the new count
+  Sift.Storage.get({
+      bucket: 'count',
+      keys: ['TOTAL']
+  }).then(function (values) {
+    console.log('hello-sift: storage returned: ', values);
+    Sift.Controller.notifyListeners('count', values[0].value);
+  });
+});
+
+// Register for specific UI events
+Sift.View.addEventListener('ncButton-pressed', function (value) {
+  console.log('hello-sift: ncButton-pressed received: ', value);
+  Sift.Storage.putUser({
+    kvs: [{key: 'NCBUTTONPRESSED', value: value}]
+  }).then(function () {
+    console.log('hello-sift: stored in user database');
+  }).catch(function (err) {
+    console.error('hello-sift: error storing in user database', err);
+  });
+});
+
+function fullAsyncHandler(resolve, reject){
+  setTimeout(function () {
+    // Asynchronous resolve
+    resolve ({
+        html: 'frontend/view1.html',
+        data: {
+          message: 'resolved asynchronously'
+        }
+      });
+    }, 1500);
+}
+
--- frontend/view1.js
+++ frontend/view1.js
@@ -1,36 +1,61 @@
 /**
- * Sift's View callbacks
- *
- * Copyright (c) 2015 Redsift Limited. All rights reserved.
+ * Hello Sift. View callbacks.
  */
 
+'use strict';
+
+/* globals document, Sift */
+var ldButton;
+var ncButton;
+var ncCount = 0;
 
 /**
- * Called by the framework when frontend/controller.js calls the resolve function
+ * Called by the framework when the loadView callback in frontend/controller.js calls the resolve function or returns a value
  *
  * Parameters:
- * value: {object} (data object returned by the load or resolve methods in the controller)
+ * @value: {
+ *  sizeClass: {
+ *      previous: {width: 'compact'|'full', height: 'compact'|'full'},
+ *      current: {width: 'compact'|'full', height: 'compact'|'full'}
+ *    },
+ *    type: 'email-detail'|'summary',
+ *    data: {object} (data object returned by the load or resolve methods in the controller)
+ *  }
  */
 Sift.View.presentView = function (value) {
-  console.log('taxi-test: presentView: ', value);
-  if (value.sizeClass) {
-    var w = document.getElementById('width');
-    w.textContent = value.sizeClass.width;
-    w.style.color = '#231F20';
+  console.log('hello-sift: presentView: ', value);
 
-    var h = document.getElementById('height');
-    h.textContent = value.sizeClass.height;
-    h.style.color = '#231F20';
+  /*
+  * Example code below can be removed
+  */
+  // Simple dom updates for the data we received after transition is over.
+  updateDOM('width', value.sizeClass.current.width);
+  updateDOM('height', value.sizeClass.current.height);
+  updateDOM('message', value.data.message);
+  updateDOM('type', value.type);
+
+  if(!ldButton) {
+    ldButton = document.getElementById('load-data');
+    ldButton.addEventListener('click', function () {
+      console.log('hello-sift: ldButton clicked');
+      Sift.Controller.loadData({key: 'TOTAL'}).then(function(result) {
+        console.log('hello-sift: loadData returned: ', result);
+        var msg = 'No data yet. Please run your DAG.';
+        if(result) {
+          msg = result + ' emails from @gmail.com in your inbox';
+        }
+        document.getElementById('data').textContent = msg;
+      });
+    });
   }
-  if (value.message) {
-    var m = document.getElementById('message');
-    m.textContent = value.message;
-    m.style.color = '#231F20';
-  }
-  if (value.type) {
-    var t = document.getElementById('type');
-    t.textContent = value.type;
-    t.style.color = '#231F20';
+
+  if(!ncButton) {
+    ncButton = document.getElementById('notify-controller');
+    ncButton.addEventListener('click', function () {
+      ncCount++;
+      console.log('hello-sift: ncButton clicked: ', ncCount);
+      Sift.View.notifyListeners('ncButton-pressed', ncCount);
+    });
   }
 };
 
@@ -40,31 +65,66 @@ Sift.View.presentView = function (value) {
  * Parameters:
  * @value: {
  *  sizeClass: {
- *    previous: {width: 'none'|'compact'|'full', height: 'none'|'list'|'compact'|'full'},
- *    current: {width: 'none'|'compact'|'full', height: 'none'|'list'|'compact'|'full'}
+ *    previous: {width: 'compact'|'full', height: 'compact'|'full'},
+ *    current: {width: 'compact'|'full', height: 'compact'|'full'}
  *  },
- *  type: 'compose'|'email-detail'|'summary'
+ *  type: 'email-detail'|'summary'
  * }
- * value.size: contains the new height and width of the view (height: text|compact|full, width: compact|full)
- * value.previous: contains the previous height and width of the view (height: text|compact|full, width: compact|full)
  */
 Sift.View.willPresentView = function (value) {
-  console.log('taxi-test: willPresentView: ', value);
-  var currWidth  = value.sizeClass.current.width;
-  var prevWidth  = value.sizeClass.previous.width;
-  var currHeigth = value.sizeClass.current.height;
-  var prevHeight = value.sizeClass.previous.height;
-  var w = document.getElementById('width');
-  if (currWidth !== prevWidth) {
-    w.textContent = prevWidth + ' > ' + currWidth;
-    w.style.color = '#ED1651';
-  }
-  var h = document.getElementById('height');
-  if (currHeigth !== prevHeight) {
-    h.textContent = prevHeight + ' > ' + currHeigth;
-    h.style.color = '#ED1651';
-  }
+  console.log('hello-sift: willPresentView: ', value);
+
+  /*
+  * Example code below can be removed
+  */
+  // Depict how often this event is fired while transitions take place
+  showTransitions('width', value.sizeClass);
+  showTransitions('height', value.sizeClass);
+
   var m = document.getElementById('message');
+  if(!m){
+    console.info('Missing dom element for example:', m);
+    return;
+  }
   m.textContent = 'will present view';
   m.style.color = '#ED1651';
 };
+
+/**
+ * Listens for 'count' events from the Controller
+ */
+Sift.Controller.addEventListener('count', function (value) {
+  console.log('hello-sift: oncount', value);
+    document.getElementById('data').textContent = 'New data: ' + value + ' emails from \'gmail.com\' in your inbox';
+});
+
+function updateDOM(elem, value){
+  var e = document.getElementById(elem);
+  if(!e){
+    console.info('Missing dom element for example:', elem);
+    return;
+  }
+  if(parent){
+    e.textContent = value;
+    e.style.color = '#231F20';
+  }
+}
+
+function showTransitions(aspect, parent){
+  if(!parent || !parent.current || !parent.previous){
+    console.error('No data for this transition');
+    return;
+  }
+  var current = parent.current[aspect];
+  var previous = parent.previous[aspect];
+  var e = document.getElementById(aspect);
+  if(!e){
+    console.info('Missing dom element for example:', aspect);
+    return;
+  }
+
+  if(current !== previous){
+    e.textContent = previous + ' > ' + current;
+    e.style.color = '#ED1651';
+  }
+}
